fb72613f54b05df59dc79aca18739dba91484180
CasperNEW <CasperNEW@inbox.ru>
2019-12-14 15:59:37 +0300

    add NewsTableView with MessageCell.xib

---
+++ VK/VIews/MessageTableView/MessageCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var message: UILabel!
+    
+    
+ 
+}
